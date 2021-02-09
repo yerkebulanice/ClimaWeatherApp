189acb5406bd72ba3a4c42f57de408428cf261e0
Kapitan <erkebulanice@gmail.com>
2021-02-09 18:59:30 +0500

    first commit

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+const kTempTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 100.0,
+);
+
+const kMessageTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 60.0,
+);
+
+const kButtonTextStyle = TextStyle(
+  fontSize: 30.0,
+  fontFamily: 'Spartan MB',
+);
+
+const kConditionTextStyle = TextStyle(
+  fontSize: 100.0,
+);
+
+const kTextFieldInputDecortion = TextStyle(
+  color: Colors.black,
+);
